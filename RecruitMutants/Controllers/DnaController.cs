af168634f52a8b2aa2527a72ea5ceeee8882bb69
Julieth Gil <Djgil@intergrupo.com>
2021-05-08 21:38:03 -0500

    Clean startup project

---
+++ RecruitMutants/Controllers/DnaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitMutants.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DnaController : ControllerBase
+    {
+        public DnaController()
+        {
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+    }
+}
---
+++ Test/Controllers/DnaControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using RecruitMutants.Controllers;
+using Xunit;
+
+namespace Test.Controllers
+{
+    public class DnaControllerTests
+    {
+        [Fact]
+        public void Post_StateUnderTest_ExpectedBehavior()
+        {
+            var dnaController = new DnaController();
+
+            dnaController.Post();
+            
+            Assert.True(false);
+        }
+    }
+}
